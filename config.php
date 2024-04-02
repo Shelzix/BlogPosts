f0c58c86e5fee87ec014777c15685a38bf07491e
PR23 <PR23@it-redmonkey.com>
2024-04-02 19:46:23 +0300

    312312313

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+define('ROOT_PATH', dirname(__FILE__));
+define('BASE_URL', 'http://qqq/');
+
+//db connect
+$conn = mysqli_connect('localhost', 'root', '', 'ideaInYou');
+
+if(!$conn){
+    die('AQUA ON ME, AAAAAAAAAAAQUA, CALL AAAAAAAAAAAAQUA. SHIT ON, SHIT ON, DANCE, DANCE, DANCE BITCH DAAAANCEEE');
+
+}
---
+++ single_post.php
@@ -0,0 +1,24 @@
+
+<?php
+ require_once('config.php');
+ require_once('includes/functions.php'); 
+ require_once('includes/headSec.php') ;
+ if(isset($_GET['topic'])){
+    $post_slug = $_GET["post_slug"];
+    $posts = getPost($post_slug);
+ }
+ ?>
+
+
+<title>LifeBlog | <?= $post[0]['title']  ?> </title>
+</head>
+
+<body>
+        <?php require_once('includes/header.php') ?>
+    <div class="container">
+        <div class="content">
+
+        </div>
+    </div>
+        <?php require_once('includes/footer.php') ?>
+</body>
